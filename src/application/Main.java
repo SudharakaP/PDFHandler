2f7c7d681e85d2757d5998f3ac0cee29e1556d4c
Sudharaka Palamakumbura <sudharakap@mun.ca>
2017-03-30 00:33:47 -0700

    Minor fix to Java runtime mismatch.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
