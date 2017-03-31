2b2cc325f6ccfa3dab8de046504616deb3abcd72
Sudharaka Palamakumbura <sudharakap@mun.ca>
2017-03-30 23:43:39 -0700

    Add support for file open dialog and displaying it in a blocking manner.

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -4,24 +4,20 @@ import java.io.File;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
 public class MainWindowController {
 	
+	@FXML AnchorPane anchorPane;
+
 	@FXML
 	private void clickOpen() throws IOException {
-		AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("MainWindow.fxml"));
-		Scene scene = new Scene(page);
-		Stage primaryStage = new Stage();
-		primaryStage.setScene(scene);
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open PDF File");
 		fileChooser.getExtensionFilters().add(new ExtensionFilter("PDF Files", "*.pdf"));
-		File selectedFile = fileChooser.showOpenDialog(primaryStage);
+		File selectedFile = fileChooser.showOpenDialog(anchorPane.getScene().getWindow());
+		
 	}
 }
