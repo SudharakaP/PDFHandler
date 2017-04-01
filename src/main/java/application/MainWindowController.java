02da09db9358f95cc8f36222ac018b600749c3ba
Sudharaka Palamakumbura <sudharakap@mun.ca>
2017-04-01 01:06:54 -0700

    Experimental: Trying to create a custom pdf viewer to integrate with the application.
    The library chosen is JPedal. Example for this is given at,
    http://files.idrsolutions.com/samplecode/org/jpedal/examples/baseviewer/BaseViewerFX.java.html

--- src/main/java/application/MainWindowController.java
+++ src/main/java/application/MainWindowController.java
@@ -3,9 +3,13 @@ package application;
 import java.io.File;
 import java.io.IOException;
 
+import org.jpedal.examples.viewer.Commands;
+import org.jpedal.examples.viewer.OpenViewerFX;
+
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 
 public class MainWindowController {
@@ -14,10 +18,23 @@ public class MainWindowController {
 
 	@FXML
 	private void clickOpen() throws IOException {
+		File selectedFile = openFileChoser();
+		openPDFFile(selectedFile);
+	}
+
+	private void openPDFFile(File selectedFile) {
+		Stage pdfStage = new Stage();
+		OpenViewerFX pdfViewer = new OpenViewerFX(pdfStage, null);
+		pdfViewer.setupViewer();
+		Object[] input =  {selectedFile};
+		pdfViewer.executeCommand(Commands.OPENFILE, input);
+	}
+
+	private File openFileChoser() {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open PDF File");
 		fileChooser.getExtensionFilters().add(new ExtensionFilter("PDF Files", "*.pdf"));
 		File selectedFile = fileChooser.showOpenDialog(anchorPane.getScene().getWindow());
-		
+		return selectedFile;
 	}
 }
