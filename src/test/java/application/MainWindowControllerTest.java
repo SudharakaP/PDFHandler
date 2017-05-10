4ded780342539a2f0d32ff521f16717d4b7de6a6
Sudharaka Palamakumbura <sudharakap@mun.ca>
2017-05-09 23:40:51 -0700

    Trying out some things with TestFx.

---
+++ src/test/java/application/MainWindowControllerTest.java
@@ -0,0 +1,20 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.stage.Stage;
+
+public class MainWindowControllerTest extends ApplicationTest {
+	@Test 
+	public void clickRotatePage(){
+		
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();		
+	}
+}
